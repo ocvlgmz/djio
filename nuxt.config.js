3905c3420a5a7c2c48ec07cc3e552117e0cb6856
oliver <oliver.carvajal@gmail.com>
2021-08-10 17:20:35 +0200

    update env file

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ import colors from 'vuetify/es5/util/colors'
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - dev',
+    titleTemplate: '%s',
     title: 'Digital Jam',
     htmlAttrs: {
       lang: 'en'
