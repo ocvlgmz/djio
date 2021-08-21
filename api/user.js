8493166d277532894e8c05b34189348d748b1f81
oliver <oliver.carvajal@gmail.com>
2021-08-21 15:15:06 +0200

    added dbConnect.js & user.js

---
+++ api/user.js
@@ -0,0 +1,22 @@
+import dbConnect from './utils/dbConnect'
+import errorHandler from './utils/errorHandler'
+import User from './models/User'
+
+export default async (req, res) => {
+    await dbConnect()
+    // const query = req.query
+    const { email, username } = req.body
+    try {
+        const user = await User.findOne({ email })
+        // console.log('uid:', user._id)
+        // const token = createToken(user._id)
+        // res.cookie('user', user, { httpOnly: true })
+        res.setHeader('Content-Type', 'application/json')
+        res.status(200).json({ user: user._id, email, username })
+    } catch (err) {
+        const errors = errorHandler(err)
+        res.status(400).json({ errors })
+    }
+}
+
+// export default hello
