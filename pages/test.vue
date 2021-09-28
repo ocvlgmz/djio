4c4eb859e18da1a19ea17a0bfda655b7a105e330
Oliver Carvajal Gómez <oliver.carvajal@gmail.com>
2021-09-28 16:51:11 +0200

    testing new remote origin

--- pages/test.vue
+++ pages/test.vue
@@ -1,18 +1,11 @@
 <template>
   <v-container>
     <h1>Test</h1>
-    <div v-for="u in users" :key="u.id">
-      <v-row>
-        <v-col>{{ u.attributes.name }}</v-col>
-        <v-col>{{ u.attributes.email }}</v-col>
-        <v-col><v-btn to="/login">login</v-btn></v-col>
-      </v-row>
-    </div>
+    <h2>{{ `Current Feature: ${this.feature}` }}</h2>
     <div v-if="isAuthenticated">
       {{ isAuthenticated }}
       <div>{{ loggedInUser.username }}</div>
       <div>{{ this.$auth.user }}</div>
-      <div>{{ this.$auth.$state }}</div>
     </div>
     <div v-else>Logged out!</div>
   </v-container>
@@ -24,6 +17,7 @@ export default {
   auth: false,
   data() {
     return {
+      feature:'vercel serverless'
     };
   },
   computed: {
