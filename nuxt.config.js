666bc2a2f2ad2a6446086cc2dc9b690ac7064582
Oliver Carvajal Gómez <oliver.carvajal@gmail.com>
2021-09-29 11:44:30 +0200

    disable axios params

--- nuxt.config.js
+++ nuxt.config.js
@@ -87,8 +87,8 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // baseURL: 'http://localhost:3000/api/',
-    baseURL: process.env.NODE_ENV === 'production' ? 'https://www.digitaljam.io/api/' : 'http://localhost:3000/api/',
-    credentials: true
+    // baseURL: process.env.NODE_ENV === 'production' ? 'https://www.digitaljam.io/api/' : 'http://localhost:3000/api/',
+    // credentials: true
   },
 
   // Content module configuration: https://go.nuxtjs.dev/config-content
