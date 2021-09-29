4ad17283ba8e5ef9a5db06d9e5334b961a83dc28
Oliver Carvajal Gómez <oliver.carvajal@gmail.com>
2021-09-29 11:13:14 +0200

    removed api excess func

--- api/routes/mailerRoutes.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require('express')
-const mailerCtrl = require('../controllers/mailerCtrl')
-
-const router = Router()
-
-router.post('/', mailerCtrl.sendForm)
-
-module.exports = router
