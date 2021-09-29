98458d21bd8b5517ae85e9b461e35713a3093d40
Oliver Carvajal Gómez <oliver.carvajal@gmail.com>
2021-09-29 23:28:13 +0200

    cleaned buildModule

--- nuxt.config.js
+++ nuxt.config.js
@@ -70,10 +70,7 @@ export default {
   
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
-    '@nuxtjs/vuetify',
-    '@nuxtjs/eslint-config',
-    '@nuxtjs/eslint-module',
-    '@babel/eslint-parser',
+    '@nuxtjs/vuetify'
   ],
   
   // Modules: https://go.nuxtjs.dev/config-modules
