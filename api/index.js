4c9a7a05d41cb62418af11dd3a07c21e55f84bb2
Oliver Carvajal Gómez <oliver.carvajal@gmail.com>
2021-10-01 18:38:38 +0200

    path corr in api index

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const jwt = require('jsonwebtoken')
 const { createToken } = require('./utils/jwtAuth')
 const errorHandler = require('./utils/errorHandler')
-const User = require('../models/User')
+const User = require('./models/User')
 
 
 // MongoDB connexion
