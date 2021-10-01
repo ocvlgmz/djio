1d703b6f84feee806d8d169adade400b0235fb2b
Oliver Carvajal Gómez <oliver.carvajal@gmail.com>
2021-10-01 15:29:24 +0200

    modified nuxt config with google fonts

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,7 +25,7 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
-    '@/assets/css/global.css', '@/assets/css/transitions.css', 'animate.css/animate.min.css'
+    '@/assets/css/global.css', '@/assets/css/googlefonts.css', '@/assets/css/transitions.css', 'animate.css/animate.min.css'
   ],
   pageTransition: 'fade',
   layoutTransition: 'fade',
