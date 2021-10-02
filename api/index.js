7d7009e2a4f6e76cdf205fe1e3cbe28aa2d53dc4
Oliver Carvajal Gómez <oliver.carvajal@gmail.com>
2021-10-02 19:02:06 +0200

    json res return in login

--- api/index.js
+++ api/index.js
@@ -30,16 +30,16 @@ app.post('/api/register', async (req, res) => {
   }
 })
 app.post('/api/login', async (req, res) => {
+  const { email, password } = req.body
   try {
-    const { email, password } = req.body
     if (!(email && password)) {
       res.status(400).send("All input is required")
     }
-    const user = await User.findOne({ email });
-    if (!user) return res.status(400).send({status:400, message: "Invalid Email or Password"});
+    const user = await User.findOne({ email })
+    if (!user) return res.status(400).json({message: "Invalid Email or Password"})
     
-    const validPassword = await bcrypt.compare(req.body.password, user.password);
-    if (!validPassword) return res.status(400).send({status:400, message: "Invalid Email or Password"});
+    const validPassword = await bcrypt.compare(password, user.password)
+    if (!validPassword) return res.status(400).json({message: "Invalid Email or Password"})
     
     const token = createToken(user._id)
     res.cookie('user', user, { httpOnly: true })
