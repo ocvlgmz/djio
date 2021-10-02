5c9b016520245dff937453143b7d9a296ed88302
Oliver Carvajal Gómez <oliver.carvajal@gmail.com>
2021-10-02 17:50:26 +0200

    missing bcrypt file

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const cookieParser = require('cookie-parser')
+const bcrypt = require('bcrypt')
 
 const mongoose = require('mongoose')
 // const jwt = require('jsonwebtoken')
