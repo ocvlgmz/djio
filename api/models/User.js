ec2277f01d283be8405a112789afc27b809a5587
Oliver Carvajal Gómez <oliver.carvajal@gmail.com>
2021-10-03 02:24:05 +0200

    modif user model

--- api/models/User.js
+++ api/models/User.js
@@ -43,26 +43,4 @@ UserSchema.pre('save', async function (next) {
     next()
 })
 const User = mongoose.model('user', UserSchema)
-module.exports = User 
-
-const PostSchema = new mongoose.Schema({
-    title: {type: String},
-    body: {type: String},
-    date: {type: Date},
-    tags:[{type: String}],
-    _author: {type: Schema.Types.ObjectId, ref: 'author'}
-});
-const Post = mongoose.model('post', PostSchema)
-module.exports = Post
-
-const AuthorSchema = new mongoose.Schema({
-    name: {type: String},
-    photo: {type: String},
-    bio: {type: String},
-    username: {type: String, index: true},
-    posts:[{type: Schema.Types.ObjectId, ref: 'post'}],
-    password: {type: String}
-});
-
-const Author = mongoose.model('author', AuthorSchema)
-module.exports = Author
+module.exports = user 
