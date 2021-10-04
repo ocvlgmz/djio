03f92342148ae4694288a75369e58852b2dd9754
Oliver Carvajal Gómez <oliver.carvajal@gmail.com>
2021-10-05 01:08:50 +0200

    req cookies in route only

--- api/index.js
+++ api/index.js
@@ -53,8 +53,8 @@ app.post('/api/login', async (req, res) => {
 //   res.status(200).json({ message: 'User logged out.' })
 // })
 app.get('/api/user', authenticateToken, (req, res) => {
-  // const user = req.cookies['user']
-  const user = req.user
+  const user = req.cookies['user']
+  // const user = req.user
   console.log(user)
   
   res.status(200).json({ user: user })
