248d9eaa45f369f5d3830d99adbad1991929be51
Oliver Carvajal Gómez <oliver.carvajal@gmail.com>
2021-10-11 18:13:07 +0200

    env

--- plugins/vue-gapi.js
+++ plugins/vue-gapi.js
@@ -2,8 +2,8 @@ import Vue from 'vue'
 import VueGapi from 'vue-gapi'
 
 Vue.use(VueGapi, {
-  apiKey: this.$config.gapiApiKey,
-  clientId: this.$config.gapiClientId,
+  apiKey: process.env.GAPI_API_KEY,
+  clientId: process.env.GAPI_CLIENT_ID,
   discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest'],
   scope: 'https://www.googleapis.com/auth/calendar',
 })
