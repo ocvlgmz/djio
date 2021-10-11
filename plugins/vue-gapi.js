2dc2673a8e9262878af9343b94d1c85a96fe6270
Oliver Carvajal Gómez <oliver.carvajal@gmail.com>
2021-10-11 19:04:42 +0200

    client id in plugin

--- plugins/vue-gapi.js
+++ plugins/vue-gapi.js
@@ -3,7 +3,7 @@ import VueGapi from 'vue-gapi'
 
 Vue.use(VueGapi, {
   apiKey: process.env.GAPI_API_KEY,
-  clientId: process.env.GAPI_CLIENT_ID,
+  clientId: '232288744858-118pjnna54b5k53btp3e2r49mde9u7ae.apps.googleusercontent.com',
   discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest'],
   scope: 'https://www.googleapis.com/auth/calendar',
 })
