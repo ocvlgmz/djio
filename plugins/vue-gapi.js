78611474954dd98e6a8417090c36663efc1b467a
Oliver Carvajal Gómez <oliver.carvajal@gmail.com>
2021-10-11 17:11:38 +0200

    gapi credentials update

--- plugins/vue-gapi.js
+++ plugins/vue-gapi.js
@@ -2,8 +2,14 @@ import Vue from 'vue'
 import VueGapi from 'vue-gapi'
 
 Vue.use(VueGapi, {
-  apiKey: 'AIzaSyAzyWuZ0DYY7kiQFQZpcz4tHoQ6qsx3l9E',
-  clientId: '232288744858-a4pva893m78n7m8ns3bkftsae61esnkn.apps.googleusercontent.com',
+  apiKey: process.env.GAPI_KEY,
+  clientId: process.env.GAPI_CLIENT_ID,
   discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest'],
   scope: 'https://www.googleapis.com/auth/calendar',
 })
+// Vue.use(VueGapi, {
+//   apiKey: 'AIzaSyAzyWuZ0DYY7kiQFQZpcz4tHoQ6qsx3l9E',
+//   clientId: '232288744858-a4pva893m78n7m8ns3bkftsae61esnkn.apps.googleusercontent.com',
+//   discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest'],
+//   scope: 'https://www.googleapis.com/auth/calendar',
+// })
